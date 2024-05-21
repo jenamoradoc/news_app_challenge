e665d92a662a96fc1cf035303884238a2eded892
David Enamorado <davidenamorado@Davids-MacBook-Pro.local>
2024-05-21 15:50:37 -0300

    Fix: Update config to fix blank gh page

---
+++ dist/assets/Footer-BPX_X8gL.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DfmWEoSy.js";const e=()=>r.jsx("footer",{children:r.jsx("p",{children:"© 2024. News App"})});export{e as F};
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/news_app_challenge',
   plugins: [react()],
   build: {
     outDir: 'dist'
